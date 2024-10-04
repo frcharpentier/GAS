8c828c8758d333df244ddbe3e27ee26123a4f08c
charpentierf <frederic.charpentier@cabotfinancial.fr>
2024-10-04 15:18:17 +0200

    ...

--- modele_lineaire.py
+++ modele_lineaire.py
@@ -2,4 +2,11 @@ import os
 from make_dataset import FusionElimination as FILT, AligDataset
 from report_generator import HTML_REPORT
 
-
+def batch_LM():
+    nom_rapport="Rapport_modèle_linéaire"
+    nom_dataset = "dataset_pipo"
+    with HTML_REPORT(nom_rapport) as R:
+        R.ligne()
+        R.titre("Dataset : %s"%nom_dataset)
+        R.texte("Effectifs avant filtrage :")
+        R.table(relations=["un", "deux", "trois"], effectifs=[1,2,3])
