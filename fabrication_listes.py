1735ff1de354d7daa11872a2298dfa043177d4ad
charpentierf <frederic.charpentier@cabotfinancial.fr>
2024-10-06 17:03:42 +0200

    ...

--- fabrication_listes.py
+++ fabrication_listes.py
@@ -573,5 +573,5 @@ def refaire_probleme():
 
 if __name__ == "__main__":
     #refaire_probleme()
-    #construire_graphes(fichier_out="./AMR_et_graphes_phrases_explct.txt", split=True)
-    construire_graphes(fichier_out="./a_tej.txt", split=False)
+    construire_graphes(fichier_out="./AMR_et_graphes_phrases_explct.txt", split=True, court=False)
+    #construire_graphes(fichier_out="./a_tej.txt", split=False, court=True)
