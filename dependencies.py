9746630da4bf00f1467a084f71902cdf70a5ed31
Frédéric Charpentier <frederic.charpentier@cabotfinancial.fr>
2025-01-23 14:30:51 +0100

    added dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,15 @@
+amr_rep_training = "../../visuAMR/AMR_de_chez_LDC/LDC_2020_T02/data/alignments/split/training"
+amr_rep_dev      = "../../visuAMR/AMR_de_chez_LDC/LDC_2020_T02/data/alignments/split/dev"
+amr_rep_test     = "../../visuAMR/AMR_de_chez_LDC/LDC_2020_T02/data/alignments/split/test"
+
+snt_rep_training = "../../visuAMR/AMR_de_chez_LDC/LDC_2020_T02/data/amrs/split/training"
+snt_rep_dev      = "../../visuAMR/AMR_de_chez_LDC/LDC_2020_T02/data/amrs/split/dev"
+snt_rep_test     = "../../visuAMR/AMR_de_chez_LDC/LDC_2020_T02/data/amrs/split/test"
+
+prefixe_alignements = "../alignement_AMR/leamr/data-release/alignments/ldc+little_prince."
+AMR_UMR_91_ROLESETS_XML = "AMR-UMR-91-rolesets.xml"
+#PROPBANK_DIRECTORY = "C:/Users/fcharpentier/Documents/Boulot/visuAMR/propbank-frames"
+PROPBANK_DIRECTORY = "../../visuAMR/propbank-frames"
+
+PROPBANK_TO_VERBATLAS = "../VerbAtlas-1.1.0/VerbAtlas-1.1.0/pb2va.tsv"
+UMR_91_ROLESETS = "./UMR_91_rolesets.tsv"
