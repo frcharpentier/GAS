46d8aae5bb1b8bdee9c6ad1d766f04e9f28520b1
charpentierf <frederic.charpentier@cabotfinancial.fr>
2025-02-03 10:54:41 +0100

    modifs autoinspect.py

--- autoinspect.py
+++ autoinspect.py
@@ -7,7 +7,9 @@ import inspect
 
 def autoinspect(fonction):
     sig = inspect.signature(fonction)
-    paires = [(nom, par) for nom, par in sig.parameters.items()]
+    paires = [(nom, par) for nom, par in sig.parameters.items() ]
+    assert all(par.kind in (inspect._ParameterKind.POSITIONAL_OR_KEYWORD, inspect._ParameterKind.VAR_KEYWORD) for _, par in paires), "Wrong function signature %s"%(fonction.__name__)
+    paires = [(nom, par) for nom, par in paires if not par.kind == inspect._ParameterKind.VAR_KEYWORD]
     assert all(nom == par.name for (nom, par) in paires)
     dico_defaut = {nom: par.default for nom, par in paires if not par.default == inspect.Parameter.empty}
     noms_argus = [nom for nom, par in paires]
@@ -41,6 +43,7 @@ def autoinspect(fonction):
         
 
         dico_argus = {k: v for k, v in dico_defaut.items()}
+        kwargus = {}
         interdits = []
         for i, v in enumerate(args):
             nom = noms_argus[i]
@@ -49,13 +52,17 @@ def autoinspect(fonction):
 
         for k, v in kwargs.items():
             assert not k in interdits, "L’argument %s de la fonction %s a été défini plus d’une fois"%(k, nom_fonction)
-            dico_argus[k] = v
+            if k in noms_argus:
+                dico_argus[k] = v
+            else:
+                kwargus[k] = v
         
         assert len(dico_argus) == len(noms_argus), "Trop ou pas assez d’arguments donnés à la fonction %s"%(nom_fonction)
         assert all(A in dico_argus for A in noms_argus), "Trop ou pas assez d’arguments donnés à la fonction %s"%(nom_fonction)
         assert all(A in noms_argus for A in dico_argus), "Trop ou pas assez d’arguments donnés à la fonction %s"%(nom_fonction)
 
         listeargs = [(A, dico_argus[A]) for A in noms_argus if not A in dico_defaut or dico_argus[A] != dico_defaut[A]]
+        listeargs.extend([(k, v) for k, v in kwargus.items()])
         exe_python = nom_fonction + "(" + ", ".join("%s=%s"%(A, repr(v)) for A, v in listeargs) + ")"
         commande = [nom_fonction]
         for k, v in listeargs:
@@ -76,4 +83,15 @@ def autoinspect(fonction):
         return fonction(*args, **kwargs)
     
     return interne
+
+
+if __name__ == "__main__":
+    def pipo(a, b, c=0, **kwargs):
+        print(a+b+c)
+        if "coucou" in kwargs:
+            print("coucou y est")
+        return a+b+c
+    
+    pipopipo = autoinspect(pipo)
+    pipopipo(2,3, DEBUG=True, coucou=5)
             
